364f4f84b9971ba6859d8f8382d8c85b1013bd92
manish bhargava <manishbhargava@manishs-MacBook-Air.local>
2025-11-10 10:57:07 +0530

    added db logs fon  proudtion

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   console.log(process.env.MONGO_URI);
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect("mongodb+srv://manish:Manish12345@cluster0.5wmrisd.mongodb.net/aura_campus_connect?retryWrites=true&w=majority&appName=Cluster0");
 };
 
 module.exports = connectDB;
